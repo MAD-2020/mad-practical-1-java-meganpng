d3eede5d2c5501e23d1527f04ac28f5180677cac
meganpng <63646364+meganpng@users.noreply.github.com>
2020-04-17 17:21:27 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,38 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter number of integers: ");
+    int a = in.nextInt();
     
+    List<Integer> data = new ArrayList<>();
+    System.out.printIn("Enter the set of integers: ");
+
+    for(int i = a; i > 0; i--){
+      Scanner mySet = new Scanner(System.in);
+      data.add(mySet);
+      
+    }
+    
+    int maxcount = -1;
+    int maxvalue = -1;
+    int mode = 0;
+    for(int i = 0; i < a; i++){
+      int count = 0;
+      for(int j = 0; j < a; j++){
+        if(data[i] = data[j]){
+          count++;
+          
+        }
+      }
+
+      if(count > maxcount){
+        maxvalue = data[i];
+        maxcount = count;
+      }
+    }
+
+    System.out.printIn
+    (maxvalue);
+
   }
 }
